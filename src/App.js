083a6e4c093e4e11bf0a41dd11d1ae2b30bf3e34
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-07 18:27:49 +0200

    citation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
+
 
 
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <QuoteCard/>
+      <QuoteCard/>
     </div>
   );
 }
