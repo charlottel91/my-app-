de033f12fa8963645686be8fd99d91967653a24b
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-07 18:00:52 +0200

    Navbar is ready

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+      <nav className = "Navbar-nav">
+          <h1>The Simpson</h1>
+      </nav>
+    );
+  }
+
+  export default Navbar;
