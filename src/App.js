85ff48fda1bc52fcb30b3a32edfd8476929cc467
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-08 11:20:58 +0200

    add props

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ const App = () => {
   return (
     <div className="App">
       <Navbar/>
-      <QuoteCard/>
+      <QuoteCard
+      image = "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+      character = "Homer Simpson"
+      quote = "In theory, Communism works! In theory."/>
+      
       <QuoteCard/>
     </div>
   );
 }
 
 export default App;
+
+
+// src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+//       alt="Nelson Muntz"
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -4,14 +4,12 @@ import React from 'react';
 const QuoteCard = () => {
     return (
       <figure className="QuoteCard">
-        <img
-      src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-      alt="Nelson Muntz"/>
+        <img {props.image} alt = {props.character}/>
         <figcaption>
         <blockquote>
-        Shoplifting is a victimless crime, like punching someone in the dark.
+        {props.quote}
         </blockquote>
-        <cite>Nelson Muntz</cite>
+        <cite>{props.character}</cite>
       </figcaption>
     </figure>
   )
