650ee68ffb5829010098bb4c5c0fcd14ba77f74d
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-24 16:43:04 +0200

    queste 7 with api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import QuoteList from './components/QuoteList';
-import QuoteForm from './components/QuoteForm.js';
-import Lamp from './components/Lamp.js'
+import QuoteCitation from './components/QuoteCitation';
+import axios from 'axios';
 
+const exemple= [{
+  quote : "I'm sleeping in the bath tub.",
+  character: "Marge Simpson",
+  image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMargeSimpson.png?1497567512205"
+}]; // rien ne s'affiche au debut !!!
 
 class App extends React.Component {
+  state = {
+    simpson : exemple[0]
+  }
+
+  getSimpson = this.getSimpson.bind(this)
+
+  getSimpson() {
+    // Send the request
+    axios.get('https://quests.wilders.dev/simpsons-quotes/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          simpson: data[0],
+        });
+    });
+  }
+
+  
+
   render(){
+    const { simpson } = this.state
     return (
       <div className="App">
-        <Navbar />
-        <QuoteForm />
-        <QuoteList />
-        <Lamp />
-        <Lamp />
+        <QuoteCitation simpson={simpson} />
+        <button type="button" onClick={this.getSimpson}>Get Simpson</button>
       </div>
     );
   }
---
+++ src/components/QuoteCitation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function QuoteCitation({ simpson }) {
+    
+
+    return (
+      <div className="QuoteCitation">
+        <img
+          src={simpson.image}
+          alt={simpson.character}
+        />
+        <ul>
+          <li>
+            Character: {simpson.character}{' '}
+          </li>
+          <li>
+            Quote: {simpson.quote}{' '}
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default QuoteCitation;
